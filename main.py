a350b19b45a504893526b42be80503781d0a52b5
AbbyH2025 <william.hendrix2025@gbwl.org>
2024-11-22 10:33:25 -0500

    heat is pretty cool, or hot. IDK

--- main.py
+++ main.py
@@ -50,21 +50,27 @@ def draw_cat_plot():
 # Function to draw Heat Map
 def draw_heat_map():
   # Clean the data
-  df_heat = None
-
+  # leave out patients where their diastolic (resting) is higher than their systolic pressure
+  df_heat = df[df['ap_lo'] <= df['ap_hi']]
+  #leave out "extreme values" by using extreme values
+  df_heat = df_heat[df['height'] >= df['height'].quantile(0.025)] 
+  df_heat = df_heat[df['height'] <= (df['height'].quantile(0.975))]
+  
+  df_heat = df_heat[df['weight'] >= df['weight'].quantile(0.025)] 
+  df_heat = df_heat[df['weight'] <= (df['weight'].quantile(0.975))]
   # Calculate the correlation matrix
-  corr = None
+  corr = df_heat.corr()
 
   # Generate a mask for the upper triangle
-  mask = None
+  mask = np.triu(corr)
 
   # Set up the matplotlib figure
-  fig, ax = None
+  fig, ax = plt.subplots(figsize=(16, 16))
 
   # Draw the heatmap with 'sns.heatmap()'
-
+  sns.heatmap(corr, mask=mask, annot=True, fmt='.2f',)
   # Do not modify the next two lines
-  fig.savefig('heatmap.png')
+  fig.savefig('heatmap.pdf')
   return fig
 
 
